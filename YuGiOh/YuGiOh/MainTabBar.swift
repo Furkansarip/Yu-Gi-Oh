0556192a3cadc542f4f05e4d05ce4d3299d768b0
Furkan Sarı <furkansarip@gmail.com>
2023-03-18 14:48:48 +0300

    Project Created & Setup Programmatic UI

---
+++ YuGiOh/YuGiOh/MainTabBar.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YuGiOh
+//
+//  Created by Furkan Sarı on 18.03.2023.
+//
+
+import UIKit
+
+final class MainTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+    }
+    
+}
+
