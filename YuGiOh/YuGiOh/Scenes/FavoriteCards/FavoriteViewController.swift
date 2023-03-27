c3582b3d400c1108a968c9a9270d218cd95acff5
Furkan Sarı <furkansarip@gmail.com>
2023-03-27 23:38:20 +0300

    FavoriteViewController UI Setup & Core Data managing

--- YuGiOh/YuGiOh/Scenes/FavoriteCards/FavoriteViewController.swift
+++ YuGiOh/YuGiOh/Scenes/FavoriteCards/FavoriteViewController.swift
@@ -11,14 +11,14 @@ protocol FavoriteViewProtocol : AnyObject {
 }
 
 class FavoriteViewController: UIViewController {
-
+    
     let tableView = UITableView(frame: .zero)
     let viewModel = FavoriteViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
         viewModel.delegate = self
-        viewModel.getFavorites()
+        title = "Favorites"
     }
     
     func configureTableView() {
@@ -27,13 +27,17 @@ class FavoriteViewController: UIViewController {
         tableView.dataSource = self
         view.addSubview(tableView)
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        viewModel.getFavorites()
+    }
+    
 }
 
 extension FavoriteViewController : UITableViewDelegate, UITableViewDataSource {
@@ -63,10 +67,35 @@ extension FavoriteViewController : UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "H"
+        let type = SectionType(rawValue: indexPath.section)
+        switch type {
+        case .monsterCard:
+            cell.textLabel?.text = viewModel.monsterList[indexPath.row].cardName
+        case .spellCard:
+            cell.textLabel?.text = viewModel.spellList[indexPath.row].cardName
+        case .trapCard:
+            cell.textLabel?.text = viewModel.trapList[indexPath.row].cardName
+        default:
+            cell.textLabel?.text = "b"
+        }
         return cell
     }
     
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        if let textColor = view as? UITableViewHeaderFooterView {
+            let type = SectionType(rawValue: section)
+            switch type {
+            case .monsterCard:
+                textColor.textLabel?.textColor = UIColor.typeColor(type: "monster")
+            case .spellCard:
+                textColor.textLabel?.textColor = UIColor.typeColor(type: "spell")
+            case .trapCard:
+                textColor.textLabel?.textColor = UIColor.typeColor(type: "trap")
+            default:
+                textColor.textLabel?.textColor = .systemBackground
+            }
+        }
+    }
 }
 
 extension FavoriteViewController : FavoriteViewProtocol {
--- YuGiOh/YuGiOh/Scenes/FavoriteCards/FavoriteViewModel.swift
+++ YuGiOh/YuGiOh/Scenes/FavoriteCards/FavoriteViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+
 protocol FavoriteViewModelProtocol {
     func getFavorites()
     func getMonster() -> Int
@@ -14,52 +15,38 @@ protocol FavoriteViewModelProtocol {
     var  delegate : FavoriteViewProtocol? { get set }
 }
 
+
 class FavoriteViewModel {
+    
     weak var delegate : FavoriteViewProtocol?
     var favoriteList = [FavoriteCardLists]()
     var filteredList = [FavoriteCardLists]()
+    var monsterList = [FavoriteCardLists]()
+    var spellList = [FavoriteCardLists]()
+    var trapList = [FavoriteCardLists]()
 }
 
+
 extension FavoriteViewModel : FavoriteViewModelProtocol {
+    
     func getMonster() -> Int {
-         favoriteList.filter({ monster in
-             if ((monster.cardType?.lowercased().contains("monster")) != nil) {
-                filteredList.append(monster)
-            }
-             return true
-        })
-        
-        return filteredList.count
+        monsterList = favoriteList.filter({$0.cardType?.lowercased().contains("monster") ?? true})
+        return monsterList.count
     }
     
     func getSpell() -> Int {
-        favoriteList.filter({ spell in
-            if ((spell.cardType?.lowercased().contains("spell")) != nil) {
-               filteredList.append(spell)
-           }
-            return true
-       })
-        
-        return filteredList.count
+        spellList = favoriteList.filter({$0.cardType?.lowercased().contains("spell") ?? true})
+        return spellList.count
     }
     
     func getTrap() -> Int {
-        favoriteList.filter({ trap in
-            if ((trap.cardType?.lowercased().contains("trap")) != nil) {
-               filteredList.append(trap)
-           }
-            return
-       })
-        return filteredList.count
+        trapList = favoriteList.filter({$0.cardType?.lowercased().contains("trap") ?? true})
+        return trapList.count
     }
-    
-   
     
     func getFavorites() {
         favoriteList = CoreDataService.shared.getFavoriteCards()
-        
         delegate?.favoritesLoaded()
         print("F",favoriteList.count)
     }
-    
 }
